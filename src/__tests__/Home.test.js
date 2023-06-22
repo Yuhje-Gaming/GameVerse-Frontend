48d74aa79fc397cb023af5bce8381725068ecc7a
Justinnkar <jkarpman01@gmail.com>
2023-06-22 16:44:57 -0700

    Home page carlousel added and button link to game index page.

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,14 +1,36 @@
-import Home from "../components/Home";
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/Home.js';
+import { BrowserRouter } from 'react-router-dom';
 
-describe('<Has a page that renders without crashing />', () => {
-  it('renders text for Home', () => {
+describe('Home Component', () => {
+  it('renders the carousel', () => {
     render(
       <BrowserRouter>
         <Home />
       </BrowserRouter>
-    )
-  })
-})
+    );
+    const carouselElement = screen.getByRole('img', { name: 'League of Legends' });
+    expect(carouselElement).toBeInTheDocument();
+  });
 
+  it('renders the "View All Games" button', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const buttonElement = screen.getByText('View All Games');
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('renders the correct number of carousel items', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const carouselItemElements = screen.getAllByRole('img');
+    expect(carouselItemElements.length).toBe(2);
+  });
+});
